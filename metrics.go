921a03391d67432fb9e1c19d0e59786b740f43e4
Davide Di Mauro <davidedimauro88@gmail.com>
2019-02-22 12:07:38 +0000

    add metrics enums and identify DB Key function

--- metrics.go
+++ metrics.go
@@ -1,19 +1,59 @@
 package sigmund
 
-import (
-	"fmt"
+import "fmt"
+
+// Metric is a type that will be used to
+// serialise the metric types
+// received from SNS
+type Metric int
+
+const (
+	lowMemory Metric = iota
+	lowCPU
+	okMemory
+	okCPU
 )
 
-func identifyMetric(m string) (string, error) {
-	var k string
-	var err error
-	switch {
-	case m == "memory":
-		k = "isLowMemory"
-	case m == "cpu":
-		k = "isLowCPU"
+var stringsToMetrics = map[string]Metric{
+	"LowMemory": lowMemory,
+	"LowCPU":    lowCPU,
+	"OkMemory":  okMemory,
+	"OkCPU":     okCPU,
+}
+
+var metricsToStrings = map[Metric]string{
+	lowMemory: "LowMemory",
+	lowCPU:    "LowCPU",
+	okMemory:  "OkMemory",
+	okCPU:     "OkCPU",
+}
+
+func (s *Sigmund) identifyDBKey() (*dbKey, error) {
+	switch metricsToStrings[s.Dynamo.Key] {
+	case "LowMemory":
+		return &dbKey{
+			"isLowMemory",
+			true,
+		}, nil
+
+	case "OkMemory":
+		return &dbKey{
+			"isLowMemory",
+			false,
+		}, nil
+
+	case "LowCPU":
+		return &dbKey{
+			"isLowCPU",
+			true,
+		}, nil
+
+	case "OkCPU":
+		return &dbKey{
+			"isLowCPU",
+			false,
+		}, nil
 	default:
-		err = fmt.Errorf("%v is an invalid parameter. Please provide either 'memory' or 'cpu' keys", m)
+		return nil, fmt.Errorf("I can't identify the Key to be used for this DB")
 	}
-	return k, err
 }
